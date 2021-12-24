af78969ecac7a31bb6a6fa367e15a8ef2288b3ee
rikumia <nori_1234@icloud.com>
2021-12-24 17:24:56 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :company, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :company, presence: true
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'が無効です。英字と数字を含めてください。', if: :password_required?
 
