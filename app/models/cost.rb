a8b5d1ef0e3bf53a1de2af297ca4209695ab6505
rikumia <nori_1234@icloud.com>
2022-01-14 11:51:51 +0900

    修正

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -6,5 +6,5 @@ class Cost < ApplicationRecord
   validates :sales, presence: true, numericality: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
   validates :cost, presence: true, numericality: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
   validates :profit, presence: true, numericality: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
-  validates :profit_was, presence: true, numericality: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+  # validates :profit_was, presence: true, numericality: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
 end
